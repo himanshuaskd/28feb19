a8be3ae63efbb530e245af3d7defee5e70b86362
himanshuaskd <himanshu.mishra_mca18@gla.ac.in>
2019-02-28 12:37:39 +0530

    add created new activity

--- app/src/main/java/com/example/a28feb19/MainActivity.java
+++ app/src/main/java/com/example/a28feb19/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a28feb19;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        final EditText editText=findViewById(R.id.editText);
+        final EditText editText3=findViewById(R.id.);
+        Button button=findViewById(R.id.button );
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s1=editText.getText().toString().trim();
+                String s2=editText3.getText().toString().trim();
+                if (s1 == "" || s2==""){
+                    Toast.makeText(MainActivity.this, "bhai Text to  dal de", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent intent=new Intent(MainActivity.class,secondActivity.class);
+                    intent.getStringExtra(s1);
+                    intent.getStringExtra(s2);
+                }
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
